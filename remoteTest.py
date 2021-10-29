394820b6c7e8b19df7bb39300f21acc2e51f08e0
akarduna <akarduna@colostate.edu>
2021-10-29 10:09:51 -0700

    added ssh stuff to local and created test script for off cs machine

---
+++ remoteTest.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import urllib3
+import os
+import csv
+
+urllib3.disable_warnings()
+def check_file(f):
+    url = "localhost:"
+    try:
+        data = json.load(f)
+    except:
+        print("Failed to load file")
+        return
+    s = []
+    for i in range(1, 28):
+        if i == 8:
+            continue
+        port = str(i).zfill(2)
+        tempUrl = url +"314"+ port + "/api/" + data['requestType']
+        x = requests.post(tempUrl, json=data, verify=False)
+        #print(json.dumps(x.text, indent=2))
+        try:
+            j = x.json()
+            if (j):
+                dist = [str(int) for int in j["distances"]]
+                s.append(dist)
+                #if(len(s) != t):
+                    #print(str(i).zfill(2) + ": " + ",".join(dist))
+        except BaseException as err:
+            print(str(i) + ": " + str(err))
+    return(s)
